52cda8bb64f71c57d4408bc6c8b81f3f0e605985
ArshadullaShaik <arshadulla_shaik@srmap.edu.in>
2026-07-09 00:50:14 +0530

    Finally major part complete

--- routes/url.js
+++ routes/url.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const minishortener = require('../controllers/url')
-
+const dbmodel = require('../modles/urldb');
+const { nanoid } = require('nanoid');
 router.post('/',async(req,res)=>{
         return minishortener(req,res)
 
@@ -15,5 +16,15 @@ router.get('/',async(req,res)=>{
 
 
 })
+
+router.get('/:nanoid',async(req,res)=>{
+        const doc = await dbmodel.findOne({ nanoid: req.params.nanoid });
+
+  if (!doc) {
+    return res.status(404).json({ message: {nanoid} });
+  }
+
+  return res.redirect(doc.redirecturl);
+});
 
 module.exports = router
